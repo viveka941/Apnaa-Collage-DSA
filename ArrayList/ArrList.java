271c3d4cb0be165ee6d7156b0c7c3a5ea7b52aa4
Vivek kumar <viveka9415@gmail.com>
2024-09-01 19:43:21 +0530

    Array List all operation -> add, get, remove, set, contains

---
+++ ArrayList/ArrList.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class ArrList{
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+    list.add(6);
+    System.out.println(list); //0(1)
+    System.out.println(list.get(4));  //0(1)
+    System.out.println(list.remove(5));
+    System.out.println(list);
+    list.set(4, 10);
+    System.out.println(list);
+    System.out.println(list.contains(10));
+  }
+}
