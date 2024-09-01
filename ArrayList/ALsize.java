4fb3e4add10c33cf3f5de6e04d6d5cbc230616aa
Vivek kumar <viveka9415@gmail.com>
2024-09-01 19:47:51 +0530

    size of ArrayList

---
+++ ArrayList/ALsize.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class ALsize {
+   public static void main(String[] args) {
+     ArrayList<Integer> list = new ArrayList<>();
+     list.add(1);
+     list.add(2);
+     list.add(3);
+     list.add(4);
+     list.add(5);
+     list.add(6);
+
+     System.out.println(list.size());
+     for(int i : list){ 
+      System.out.print(i+" ");
+
+     }
+   }
+}
