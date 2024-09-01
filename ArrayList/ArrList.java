5c7d3ae822d41b89602ba586856261df94b2371c
Vivek kumar <viveka9415@gmail.com>
2024-09-01 19:44:24 +0530

    second method add element

--- ArrayList/ArrList.java
+++ ArrayList/ArrList.java
@@ -9,6 +9,7 @@ class ArrList{
     list.add(4);
     list.add(5);
     list.add(6);
+    list.add(2,200);//0(n)
     System.out.println(list); //0(1)
     System.out.println(list.get(4));  //0(1)
     System.out.println(list.remove(5));
