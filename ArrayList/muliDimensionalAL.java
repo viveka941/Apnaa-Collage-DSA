dbbe7c6c44af1d07192f9fd0196a325d583c3cfb
Vivek kumar <viveka9415@gmail.com>
2024-09-03 22:14:09 +0530

    most water contain multi dimensoinal array pair sum , pair sum two approach

---
+++ ArrayList/muliDimensionalAL.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class muliDimensionalAL {
+  public static void main(String[] args) {
+    ArrayList<ArrayList<Integer>> mainList = new ArrayList<>();
+    ArrayList<Integer> List = new ArrayList<>();
+    List.add(1); List.add(2);
+    mainList.add(List);
+
+    ArrayList<Integer> list2 = new ArrayList<>();
+    list2.add(3);list2.add(4);
+    mainList.add(list2);
+
+
+    for(int i=0; i<mainList.size(); i++){
+      ArrayList<Integer> currList = mainList.get(i);
+      for(int j=0; j<currList.size(); j++){
+        System.out.print(currList.get(j)+" ");
+      }
+      System.out.println();
+    }
+
+    System.out.println(mainList);
+  }
+
+}
