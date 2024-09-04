86ae1471d7154e4d1f473ebc45f60db0dbd8a12f
Vivek kumar <viveka9415@gmail.com>
2024-09-04 20:02:14 +0530

    collectionframework with LL

---
+++ Linked List/collection.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class collection {
+  public static void main(String[] args) {
+    LinkedList<Integer> ll = new LinkedList<>();
+    ll.add(2);
+    ll.add(4);
+    ll.add(6);
+    ll.add(9);
+    System.out.println(ll);
+  }
+}
