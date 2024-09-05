1f7d795a8ae167e97eecdc261cec1ab093a5472a
Vivek kumar <viveka9415@gmail.com>
2024-09-05 09:29:53 +0530

    Stack implimentation with collection framework and using ArrayList

--- Linked List/collection.java
+++ Linked List/collection.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class collection {
+  public static void sort(LinkedList<Integer> ll) {
+    
+  }
+
   public static void main(String[] args) {
     LinkedList<Integer> ll = new LinkedList<>();
-    ll.add(2);
+    ll.add(12);
     ll.add(4);
     ll.add(6);
     ll.add(9);
---
+++ Stack/basicStack.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+
+class basicStack{
+  public static void main(String[] args) {
+    Stack<Integer> s= new Stack<>();
+    s.push(34);
+    s.push(35);
+    s.push(33);
+    s.push(14);
+    System.out.println(s.);
+  }
+}
