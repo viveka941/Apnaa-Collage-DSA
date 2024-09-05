9fe77e0239174fb99a8354aa4a09b682c898a1e5
Vivek kumar <viveka9415@gmail.com>
2024-09-05 23:12:04 +0530

    Queue Impliment with ArrayDeque

--- Queue/QueueImp.java
+++ Queue/QueueImp.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 public class QueueImp {
   public static void main(String[] args) {
-    Queue<Integer> s = new LinkedList<>();
+    // Queue<Integer> s = new LinkedList<>();
+    Queue<Integer> s = new ArrayDeque<>();
     s.add(1);
     s.add(2);
     s.add(3);
