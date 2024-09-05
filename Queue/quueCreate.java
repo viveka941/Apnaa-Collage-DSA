bbc1242e911f6cdb5a996bc563dcbd6e48da3ed1
Vivek kumar <viveka9415@gmail.com>
2024-09-05 21:45:06 +0530

    add remove peek

---
+++ Queue/quueCreate.java
@@ -0,0 +1,58 @@
+public class quueCreate {
+  static class Queue {
+    static int arr[];
+    static int size;
+    static int rear;
+
+    Queue(int n) {
+      arr = new int[n];
+      size = n;
+      rear = -1;
+    }
+
+    public static boolean isEmpty() {
+      return rear == -1;
+    }
+
+    // add
+    public static void add(int data) {
+      if(rear==size-1){
+        System.out.println("Queue is full");
+        return;
+      }
+      rear=rear+1;
+      arr[rear]=data;
+    }
+    // remove
+    public static int remove(){
+      if(isEmpty()){
+        System.out.println("Queue is empty");
+        return -1;
+      }
+      int fornt = arr[0];
+      for(int i=0; i<rear; i++){
+        arr[i]=arr[i+1];
+      }
+      rear = rear-1;
+      return fornt;
+    }
+    public static int peek(){
+      if(isEmpty()){
+        System.out.println("Queue is empty");
+        return -1;
+      }
+      return arr[0];
+    }
+  }
+
+  public static void main(String[] args) {
+    Queue q = new Queue(5);
+    q.add(5);
+    q.add(6);
+    q.add(7);;
+    while (!q.isEmpty()) {
+      System.out.println(q.peek());
+      q.remove();
+    }
+  }
+}
