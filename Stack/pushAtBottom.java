32096f4263742325cf0b0e883f1f7f088a0dc0f7
Vivek kumar <viveka9415@gmail.com>
2024-09-05 10:37:01 +0530

    Reverse Stack Element

--- Stack/pushAtBottom.java
+++ Stack/pushAtBottom.java
@@ -1,25 +1,42 @@
 import java.util.*;
+
 public class pushAtBottom {
-  public static void pushAtBottom(Stack<Integer> s, int data){
-    if(s.isEmpty()){
+  public static void pushAtBottom(Stack<Integer> s, int data) {
+    if (s.isEmpty()) {
       s.push(data);
       return;
     }
-    int top=s.pop();
+    int top = s.pop();
     pushAtBottom(s, data);
     s.push(top);
+  }
+
+  public static void revers(Stack<Integer> s) {
+    if (s.isEmpty()) {
+      return;
+    }
+    int top = s.pop();
+    revers(s);
+    pushAtBottom(s, top);
+  }
+
+  public static void printStack(Stack<Integer> s) {
+    
+    while (!s.isEmpty()) {
+      System.out.print(s.peek() +" ");
+      s.pop();
+    }
   }
 
   public static void main(String[] args) {
     Stack<Integer> s = new Stack<>();
     s.push(1);
     s.push(2);
-    s.push(3)
-    ;
-    pushAtBottom(s, 05);
-    while (!s.isEmpty()) {
-      System.out.println(s.peek());
-      s.pop();
-    }
+    s.push(3);
+    // 3,2,1
+   
+    revers(s);
+    printStack(s);
+    //1,2,3
   }
 }
